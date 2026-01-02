4faebe3531046a9a73c315506a7cf7e5d36d426a
RAHUL KUMAR <rahulrathore844124@gmail.com>
2026-01-02 14:51:20 +0530

    Added maximumElement.java to find max element in array
    Implement a program to find the maximum element in an array.

---
+++ Array/maximumElement.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class maximumElement {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Size: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter Values: ");
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int max = arr[0];
+        for(int i=0;i<n;i++) {
+            if(arr[i]>max)
+            max = arr[i];
+        }
+        System.out.print("Maximum Element: "+ max);
+        
+    }
+}
