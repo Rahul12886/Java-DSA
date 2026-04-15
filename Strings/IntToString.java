1be6c8bc59f1aac81e02126df5260e4fdf44c2e4
RAHUL KUMAR <rahulrathore844124@gmail.com>
2026-04-15 23:00:19 +0530

    IntToString.java
    Added output comment for clarity.

---
+++ Strings/IntToString.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class IntToString {
+    public static void main(String args[]) {
+        int n = 69;
+        String str = Integer.toString(n);
+        System.out.print(str);
+    }
+}
+
+
+/*
+  -----  OUTPUT  -----
+  69  ->  it is a string
+*/
