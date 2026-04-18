08a0f08d5d556b34d481d04257b91d92fce08e2c
RAHUL KUMAR <rahulrathore844124@gmail.com>
2026-04-18 21:18:17 +0530

    Add example outputs for substring method
    Added comments showing example outputs of substring method.

---
+++ Strings/Substring.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Substring {
+    public static void main(String args[]) {
+        String s = "Rahul";
+        System.out.println(s.substring(0));
+        
+        String str = "Kumar";
+        System.out.println(str.substring(2));
+        
+        String n = "RahulKumar";
+        System.out.println(n.substring(2,6));
+    }
+}
+
+
+/*
+  -----  OUTPUT  -----
+  Rahul
+  mar
+  hulK
+
+*/
